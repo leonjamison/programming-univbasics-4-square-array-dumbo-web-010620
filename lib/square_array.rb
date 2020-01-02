c4652ffffb38b7fb35487e18cea81a888610d8c3
Leon Jamison <leonjamison@icloud.com>
2020-01-02 17:12:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,17 @@
+# def square_array(array)
+#   array = [1,2,3]
+#   while array.each do |i|
+#   puts array
+#   square_array ** 2
+#   end
+# end
+
 def square_array(array)
-  array = [1,2,3]
-  while array.each do |i|
-  puts array
-  square_array ** 2
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
   end
+  squared
 end
